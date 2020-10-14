a9177ed88574e9790268a80151f61739d3a524e2
tjaRoxasXIII <tjaroguetech@gmail.com>
2020-10-14 12:55:17 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,15 @@
 class Student < ActiveRecord::Base
+  
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def is_active?
+    if self.active 
+       true
+    else
+      false
+    end
+  end
+  
 end
